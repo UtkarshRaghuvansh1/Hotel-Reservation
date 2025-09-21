e1e9e05d16ba249c032694495e478fbcd8cc345b
UtkarshRaghuvanshi <UtkarshRaghuvanshi@fico.com>
2025-09-21 15:26:01 +0530

    Showing all reservatiin List

--- hotel-app/src/app/reservation-list/reservation-list.component.ts
+++ hotel-app/src/app/reservation-list/reservation-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservations } from '../models/reservations';
 
 @Component({
   selector: 'app-reservation-list',
   templateUrl: './reservation-list.component.html',
   styleUrls: ['./reservation-list.component.css']
 })
-export class ReservationListComponent {
+export class ReservationListComponent implements OnInit {
+  reservations: Reservations[] = [];
+
+  constructor(private reservationService: ReservationService) {}
+
+  ngOnInit(): void {
+    this.reservations = this.reservationService.getReservations();
+  }
 
 }
